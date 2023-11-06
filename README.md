7597be9f4a75a38ca44954c18ecf0f247e8eaa80
Tchikaya Ariel <tchikayaline@gmail.com>
2023-11-06 11:00:55 +0100

    The project type message

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Global-project
+# Global-projectThis is a Bash project.
+
+To run the starter script, execute the following command on your terminal:
+./starter.sh
+If you renamed your script, replace 'starter.sh' with the actual name of your script.
--- init_project.sh
+++ init_project.sh
@@ -1,19 +1,62 @@
 #!/bin/bash
 
-echo "Hello user, welcome to our global project initialization script !!!"
-echo "Hope you will like it"
-echo "*******************************************************************"
-echo ""
+# Project type
+project_type() {
+  echo "Please select which project you would like to work on:"
+  echo "1. Python"
+  echo "2. Bash"
+  echo "Enter the number corresponding to your choice: "
+  read -r choice
 
-echo "Please enter your project name you would like to create:"
-echo "*******************************************************************"
-read -r name
-echo ""
-echo "Ohhh pretty cool name! So you want to create a project named $name..."
-echo "***************************************************************************"
-echo "Well, let's proceed."
+  if [[ $choice -eq 1 ]]; then
+    # Create a Python starter file
+    echo "Creating a Python starter file..."
+    touch starter.py
+    echo "print('Hello user, this file was created for your Python project')" >> starter.py
 
-mkdir "$name"
-echo "**************************************************************************"
-echo "A directory named $name was created for this project."
-cd "$name" || exit
+    # Set up a virtual environment
+    echo "Setting up a virtual environment..."
+    python3 -m venv venv
+    source venv/bin/activate
+    echo "Virtual environment created and activated."
+
+  elif [[ $choice -eq 2 ]]; then
+    # Create a Bash starter file
+    echo "Creating a Bash starter file..."
+    touch starter.sh
+    echo "#!/bin/bash" >> starter.sh
+    echo "echo 'Hello user, this file was created for your Bash script project'" >> starter.sh
+
+    # Generate a README file with run instructions
+    echo "Generating a README file..."
+    touch README.md
+    echo "This is a Bash project." >> README.md
+    echo "" >> README.md
+    echo "To run the starter script, execute the following command on your terminal:" >> README.md
+    echo "./starter.sh" >> README.md
+    echo "If an error message pops up saying permission denied, then meaning you don't have the xecution permission">> README.md
+    echo "./So inorder to solve this issue you will run this command 'chmod 777 starter.sh' then './starter.sh' to execute" >> README.md
+    echo "If you renamed your script, replace 'starter.sh' with the actual name of your script." >> README.md
+  else
+    echo "Invalid choice. please enter a valid choice..."
+    echo "Will you like to quit?"
+    echo "1. YES"
+    echo "2. NO"
+    echo "please enter a number which corresponds to your choice"
+    read -r choice2
+   if [[ $choice2 -eq 1 ]]; then
+    exit 1
+   elif [[ $choice2 -eq 2 ]]; then
+    project_type
+    return
+	else
+	echo "Invalid number, exiting..."
+	exit 1
+	fi
+  fi
+
+  echo "Project initialization completed successfully."
+}
+
+# Call the project_type function to start the project initialization
+project_type
