245a3a45a997ceb4aed862bde617160b436b35b5
arielpetit <111531313+Arielpetit@users.noreply.github.com>
2023-11-06 12:08:33 +0100

    Update init_project.sh

--- init_project.sh
+++ init_project.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+echo "Hello user, welcome to our global project initialization script !!!"
+echo "Hope you will like it"
+echo "*******************************************************************"
+echo ""
 
+echo "Please enter your project name you would like to create:"
+echo "*******************************************************************"
+read -r name
+echo ""
+echo "Ohhh pretty cool name! So you want to create a project named $name..."
+echo "***************************************************************************"
+echo "Well, let's proceed."
+
+mkdir "$name"
+echo "**************************************************************************"
+echo "A directory named $name was created for this project."
+cd "$name" || exit
 # Project type
 project_type() {
   echo "Please select which project you would like to work on:"
