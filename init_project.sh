45dcf56ac0569998f879194e560269ee19bedcec
Tchikaya Ariel <tchikayaline@gmail.com>
2023-11-06 10:12:25 +0100

    latest changes

--- init_project.sh
+++ init_project.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+
 echo "Hello user, welcome to our global project initialization script !!!"
 echo "Hope you will like it"
 echo "*******************************************************************"
-echo " "
+echo ""
 
-echo "Please enter your project name you will like to carry out "
+echo "Please enter your project name you would like to create:"
 echo "*******************************************************************"
-read name
-echo " "
-echo "Ohhh pretty cool name, So You will like to carry out a project named $name..."
+read -r name
+echo ""
+echo "Ohhh pretty cool name! So you want to create a project named $name..."
 echo "***************************************************************************"
-echo "Well lets proceed"
+echo "Well, let's proceed."
 
+mkdir "$name"
+echo "**************************************************************************"
+echo "A directory named $name was created for this project."
+cd "$name" || exit
