5cf910ff85edfd173c2d78ab18ab45db838b2bb4
Tchikaya Ariel <tchikayaline@gmail.com>
2023-11-06 11:40:02 +0100

    fot the README_GUIDE

--- init_project.sh
+++ init_project.sh
@@ -13,6 +13,16 @@ project_type() {
     echo "Creating a Python starter file..."
     touch starter.py
     echo "print('Hello user, this file was created for your Python project')" >> starter.py
+	# Generate a README file with run instructions
+    echo "Generating a README file for the bash project..."
+    touch README.md
+    echo "Dear user thankyou for using our GLBAL PROJECT INITIALIZATION" >> README.md
+    echo "Here are some guidelines on how you are going to start working with the project" >> README.md
+    echo "for the python project, a directory and virtual environment was created and activated for your project to be in a more adequate space of work" >>README.md
+    echo "It might require you to install some libaries to carry out your project efficientely" >> README
+    echo "We eventually created a python file for you to type your codes in a good environment" >> README
+    echo "To run your code in the terminal when finished saving it you can use the command 'python <name of file>' or python3 <name of file>" >> README
+    echo "We hope this few guide lines help you to achieve a better understanding on how to work with your project and achieve the best, GOOD LUCK!!!"
 
     # Set up a virtual environment
     echo "Setting up a virtual environment..."
@@ -28,7 +38,7 @@ project_type() {
     echo "echo 'Hello user, this file was created for your Bash script project'" >> starter.sh
 
     # Generate a README file with run instructions
-    echo "Generating a README file..."
+    echo "Generating a README file for the bash project..."
     touch README.md
     echo "This is a Bash project." >> README.md
     echo "" >> README.md
@@ -37,6 +47,14 @@ project_type() {
     echo "If an error message pops up saying permission denied, then meaning you don't have the xecution permission">> README.md
     echo "./So inorder to solve this issue you will run this command 'chmod 777 starter.sh' then './starter.sh' to execute" >> README.md
     echo "If you renamed your script, replace 'starter.sh' with the actual name of your script." >> README.md
+    echo "Dear user thankyou for using our GLBAL PROJECT INITIALIZATION" >> README>
+    echo "Here are some guidelines on how you are going to start working with the "
+    echo "for the bash project, a directory was actually  created for the project to be carried out in it "
+    echo " "
+    echo "We eventually created a bash  file for you to type your codes in a good and adequate environment"
+    echo "To run your code in the terminal when finished saving it you can use the the following command './<name of file>'"
+    echo "Hope this guide line give you a brief and clear understanding  on how to work with your project and achieve the best, GOOD LUCK!!!"
+
   else
     echo "Invalid choice. please enter a valid choice..."
     echo "Will you like to quit?"
@@ -58,5 +76,4 @@ project_type() {
   echo "Project initialization completed successfully."
 }
 
-# Call the project_type function to start the project initialization
-project_type
+# Call the project_type function to s
