a29fc12071a8ddb230bebec4b004db4600017ed3
Tchikaya Ariel <tchikayaline@gmail.com>
2023-11-06 09:44:29 +0100

    code project-name

---
+++ init_project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Hello user, welcome to our global project initialization script !!!"
+echo "Hope you will like it"
+echo "*******************************************************************"
+echo " "
+
+echo "Please enter your project name you will like to carry out "
+echo "*******************************************************************"
+read name
+echo " "
+echo "Ohhh pretty cool name, So You will like to carry out a project named $name..."
+echo "***************************************************************************"
+echo "Well lets proceed"
+
